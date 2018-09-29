2e2be990a77b884ad41fb0cb53bb08f874ef37bf
moran.li@usask.ca <moran.li@usask.ca>
2018-09-29 13:16:00 -0400

    add two more algorithm

--- linkedlist/ck189/remove duplicate from unsorted list.java
+++ linkedlist/ck189/remove duplicate from unsorted list.java
@@ -1,7 +1,56 @@
+import java.util.Iterator;
+
 class solution{
+  /**
+   * double loop algorithm
+   * Time complexity: O(n^2)
+   * Space complexity: O(1)
+   * @param list list that need to remove duplicate element
+   * @return new length of list
+   * for each element in list
+   * loop through all the element after
+   * if find duplicate, remove it
+   */
+  public int doubleLoopSolution(LinkedList<Integer> list){
+    for(int i=0;i<list.size();i++){
+      for(int j=i+1;j<list.size();j++){
+        if(list.get(i) == list.get(j)){
+          list.remove(j);
+        }
+      }
+    }
+    return list.szie();
+  }
+  /**
+   * set algorithm
+   * Time complexity: O(n)
+   * Space complexity: O(2)
+   * @param list list that need to remove duplicate element
+   * @return new length of list
+   * for each element in list
+   * add it to set, remove it from list
+   * bacause set do not have duplicate element
+   * get all different elements from set 
+   * add to list again
+   */
+  public int setSolution(LinkedList<Integer> list){
+    HashSet<Integer> set = new HashSet();
+    for(int i=0;i<list.size();i++){
+      set.put(list.get(i));
+      list.remove(i);
+    }
+    Iterator<E> iterator = set.iterator();
+    while(iterator.hasNext()){
+      list.add(iterator.next());
+    }
+    return list.size();
+  }
   /**
    * solution from leetcode 26
    * two pointer algoritm
+   * Time complexity: O(n*log(n))
+   * use of Collection.sort()
+   * Space complexity: O(1)
    * @param list list that need to remove duplicate element
    * @return new length of list
    * loop through list
