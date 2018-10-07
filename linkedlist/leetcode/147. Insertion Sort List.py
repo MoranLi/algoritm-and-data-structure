d62f240fbafbf4c82c5e805f57cba86718f8b2f2
moran.li@usask.ca <moran.li@usask.ca>
2018-10-06 22:31:28 -0400

    update insert sort for null case

--- linkedlist/leetcode/147. Insertion Sort List.py
+++ linkedlist/leetcode/147. Insertion Sort List.py
@@ -4,30 +4,35 @@ class ListNode(object):
          self.val = x
          self.next = None
 
-def insertionSortList(head):
-    """
-    :type head: ListNode
-    :rtype: ListNode
-    """
-    nlist = []
-    p1 = head
-    while p1 != None:
-        nlist.append(p1.val)
-        p1 = p1.next
-    for index in range(1, len(nlist)):
-        currentvalue = nlist[index]
-        position = index
-        while position > 0 and nlist[position - 1] > currentvalue:
-            nlist[position] = nlist[position - 1]
-            position = position - 1
-        nlist[position] = currentvalue
-    h2 = ListNode(nlist[0])
-    x = h2
-    for i in range(1,len(nlist)):
-        y = ListNode(nlist[i])
-        x.next = y
-        x = y
-    return h2
+
+class Solution(object):
+    def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return None
+        nlist = []
+        p1 = head
+        while p1 != None:
+            nlist.append(p1.val)
+            p1 = p1.next
+        for index in range(1, len(nlist)):
+            currentvalue = nlist[index]
+            position = index
+            while position > 0 and nlist[position - 1] > currentvalue:
+                nlist[position] = nlist[position - 1]
+                position = position - 1
+            nlist[position] = currentvalue
+        h2 = ListNode(nlist[0])
+        x = h2
+        for i in range(1, len(nlist)):
+            y = ListNode(nlist[i])
+            x.next = y
+            x = y
+        return h2
+
 
 z = ListNode(0)
 a = ListNode(1)
