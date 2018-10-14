0ea73d72d80551158b36df31f51f0d2fbc61d6a3
moran.li@usask.ca <moran.li@usask.ca>
2018-10-14 09:57:23 -0400

    post order traversal

--- Tree/leetcode/TreeNode.py
+++ Tree/leetcode/TreeNode.py
@@ -1,6 +1,11 @@
 # Definition for a binary tree node.
 class TreeNode(object):
-     def __init__(self, x):
+    def __init__(self, x):
          self.val = x
          self.left = None
          self.right = None
+
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
